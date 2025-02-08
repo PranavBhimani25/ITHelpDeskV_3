b096e34cd7af3d8fcf968cdd7aaf02973a5babce
PranavBhimani25 <118831945+PranavBhimani25@users.noreply.github.com>
2025-02-08 21:41:06 +0530

    Add project files.

---
+++ ITHelpDeskV_3/Data/ITHelpDeskV_3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ITHelpDeskV_3.Model;
+
+namespace ITHelpDeskV_3.Data
+{
+    public class ITHelpDeskV_3Context : DbContext
+    {
+        public ITHelpDeskV_3Context (DbContextOptions<ITHelpDeskV_3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ITHelpDeskV_3.Model.UserLogin> UserLogin { get; set; } = default!;
+    }
+}
---
+++ ITHelpDeskV_3/Model/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿namespace ITHelpDeskV_3.Model
+{
+    public class UserLogin
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Type { get; set; }
+    }
+}
